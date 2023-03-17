3e28cb85f67185ea6261a471e62c801fc64a4e18
InfoJavaF <florian.belin@itech.fr>
2023-03-17 14:51:43 +0100

    Commit salarié 2

--- src/main/java/com/inti/model/Hotel.java
+++ src/main/java/com/inti/model/Hotel.java
@@ -1,9 +1,12 @@
 package com.inti.model;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -19,5 +22,7 @@ public class Hotel {
 	private int idH;
 	private String nom; 
 	
+	@OneToMany (mappedBy = "hotel", targetEntity = Salarie.class)
+	private List<Salarie> listeSalarie;
 	
 }

2b83fd4f76b6a7f133ddc11cd2ee3cf7b31a1e31
Thaou15 <thaoura.mhoma@laposte.net>
2023-03-17 15:09:49 +0100

    client

--- src/main/java/com/inti/repository/IClientRepository.java
+++ src/main/java/com/inti/repository/IClientRepository.java
@@ -1,13 +1,6 @@
 package com.inti.repository;
 
-<<<<<<< HEAD
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import com.inti.model.Client;
-@Repository
-public interface IClientRepository extends JpaRepository<Client, Integer> {
-=======
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -16,6 +9,6 @@ import com.inti.model.Client;
 
 @Repository
 public interface IClientRepository extends JpaRepository<Client, Integer>{
->>>>>>> branch 't' of https://github.com/zwileo/TP2SpringSecurity.git
+
 
 }

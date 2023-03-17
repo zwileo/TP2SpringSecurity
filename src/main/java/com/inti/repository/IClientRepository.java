cfe09f62fc9783af2fb2d2e38abe30071870af79
Niels <Niels@DESKTOP-48UCLNL>
2023-03-17 14:51:14 +0100

    Creation repository client

--- src/main/java/com/inti/repository/IClientRepository.java
+++ src/main/java/com/inti/repository/IClientRepository.java
@@ -1,5 +1,12 @@
 package com.inti.repository;
 
-public interface IClientRepository {
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.inti.model.Client;
+
+@Repository
+public interface IClientRepository extends JpaRepository<Client, Integer>{
 
 }

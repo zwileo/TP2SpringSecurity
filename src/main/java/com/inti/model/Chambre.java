a7abeaed1e56d96badc25fb163b326a85160b1f4
Coralie2798 <coralie.bodiguel@gmail.com>
2023-03-17 14:34:20 +0100

    class chambre

---
+++ src/main/java/com/inti/model/Chambre.java
@@ -0,0 +1,24 @@
+package com.inti.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity @Table
+@Data @NoArgsConstructor @AllArgsConstructor
+public class Chambre {
+
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private int numero;
+	private double superficie;
+	private int etage;
+	private boolean disponibilite;
+	private double prix;
+}

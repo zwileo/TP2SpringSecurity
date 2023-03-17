7e17ea3f5ad4c5feb93489086937e26221ab6b66
Niels <Niels@DESKTOP-48UCLNL>
2023-03-17 14:12:59 +0100

    premier commit

---
+++ src/main/java/com/inti/Tp2SpringSecurity2Application.java
@@ -0,0 +1,13 @@
+package com.inti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2SpringSecurity2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2SpringSecurity2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/inti/Tp2SpringSecurity2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2SpringSecurity2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
